6fbc4cf4ef47220ef98ce3c93305cdd203c15357
sunin457 <sunin457@gmail.com>
2022-10-28 16:44:21 +0700

    first

---
+++ lib/bloc/movie_list_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:network/network.dart';
+import 'package:repository/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class MovieListBloc {
+  final _repository = Repository();
+  final _movieFetcher = PublishSubject<Movie>();
+
+  Stream<Movie> get allMovie => _movieFetcher.stream;
+
+  fetchAllMovie() async {
+    Movie movie = await _repository.fetchAllMovie();
+    _movieFetcher.sink.add(movie);
+  }
+
+  dispose() {
+    _movieFetcher.close();
+  }
+}
